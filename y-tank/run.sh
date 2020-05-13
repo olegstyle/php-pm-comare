4a6b75f9685a870850e24e3023d01e68f69f07af
Oleh Borysenko <oleg.borisenko@blockchainhouse.co>
2020-05-13 22:10:25 +0200

    monitoring

--- y-tank/run.sh
+++ y-tank/run.sh
@@ -1,17 +1,17 @@
 port=8001
-count=10
+rps=10
 
 [ -z "$1" ] && echo "Default port set: ${port}" || port=$1 && echo "Port set: ${port}"
-[ -z "$2" ] && echo "Default count set: ${count}" || count=$2 && echo "Count set: ${count}"
+[ -z "$2" ] && echo "Default RPS set: ${rps}" || rps=$2 && echo "RPS set: ${rps}"
 
 cp template.yaml load.yaml
 
 sed -i '' "s/{PORT}/${port}/g" load.yaml
-sed -i '' "s/{COUNT}/${count}/g" load.yaml
+sed -i '' "s/{COUNT}/${rps}/g" load.yaml
 
 docker run \
     --rm \
     -v $(pwd):/var/loadtest \
-    -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
+    -v $(pwd)/.ssh:/root/.ssh \
     --net host \
     -it direvius/yandex-tank
